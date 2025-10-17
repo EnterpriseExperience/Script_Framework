b4ffe8daac6e7bab678de28506c16c6a45d09a99
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-10-17 03:39:47 -0400

    Update GlobalEnv_Framework.lua

--- GlobalEnv_Framework.lua
+++ GlobalEnv_Framework.lua
@@ -1,11 +1,24 @@
 if not getgenv().Game then
     getgenv().Game = cloneref and cloneref(game) or game
 end
+wait(0.1)
 getgenv().AllClipboards = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
 getgenv().httprequest_Init = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 get_http = getgenv().httprequest_Init or (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 getgenv().queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+getgenv().JobID = getgenv().Game.JobId
+getgenv().PlaceID = getgenv().Game.PlaceId
 queueteleport = getgenv().queueteleport
+wait(0.1)
+local function Service_Wrap(service)
+   if cloneref then
+      return cloneref(getgenv().Game:GetService(service))
+   else
+      return getgenv().Game:GetService(service)
+   end
+end
+wait(0.1)
+getgenv().Service_Wrap = Service_Wrap
 
 local function init_services()
    local services = {
@@ -263,3 +276,128 @@ getgenv().print_executor = function()
 
    return print(executor_string)
 end
+
+local function Get_Char(Player)
+   if not Player or not Player.Character then
+      local Char = nil
+      local conn
+      conn = Player.CharacterAdded:Connect(function(c)
+         Char = c
+      end)
+
+      repeat task.wait() until Char or not Player.Parent
+      if conn then conn:Disconnect() end
+      return Char
+   end
+   return Player.Character
+end
+
+wait(0.1)
+getgenv().Terrain = getgenv().Workspace.Terrain or getgenv().Workspace:FindFirstChild("Terrain")
+getgenv().Camera = getgenv().Workspace.CurrentCamera
+getgenv().LocalPlayer = getgenv().Players.LocalPlayer
+getgenv().Backpack = getgenv().LocalPlayer:WaitForChild("Backpack") or getgenv().LocalPlayer:FindFirstChild("Backpack") or getgenv().LocalPlayer:FindFirstChildOfClass("Backpack") or getgenv().LocalPlayer:FindFirstChildWhichIsA("Backpack")
+getgenv().PlayerGui = getgenv().LocalPlayer:WaitForChild("PlayerGui") or getgenv().LocalPlayer:FindFirstChild("PlayerGui") or getgenv().LocalPlayer:FindFirstChildOfClass("PlayerGui") or getgenv().LocalPlayer:FindFirstChildWhichIsA("PlayerGui")
+getgenv().PlayerScripts = getgenv().LocalPlayer:WaitForChild("PlayerScripts") or getgenv().LocalPlayer:FindFirstChild("PlayerScripts")
+getgenv().Character = Get_Char(getgenv().LocalPlayer) or getgenv().LocalPlayer.Character or getgenv().LocalPlayer.CharacterAdded:Wait()
+wait(0.1)
+
+local function Get_Char(Player)
+   if not Player or not Player.Character then
+      local Char = nil
+      local conn
+      conn = Player.CharacterAdded:Connect(function(c)
+         Char = c
+      end)
+
+      repeat task.wait() until Char or not Player.Parent
+      if conn then conn:Disconnect() end
+      return Char
+   end
+   return Player.Character
+end
+
+local function SafeGetHumanoid(char)
+	local hum = char:FindFirstChildWhichIsA("Humanoid")
+
+	if hum and hum:IsA("Humanoid") then
+		return hum
+	else
+		return char:WaitForChild("Humanoid", 5)
+	end
+end
+
+local function SafeGetHead(char)
+	local head = char:FindFirstChild("Head")
+	if head and head:IsA("BasePart") then
+		return head
+	else
+		return char:WaitForChild("Head", 5)
+	end
+end
+
+local function SafeGetHRP(char)
+	local hrp = char:FindFirstChild("HumanoidRootPart")
+	if hrp and hrp:IsA("BasePart") then
+		return hrp
+	else
+		return char:WaitForChild("HumanoidRootPart", 5)
+	end
+end
+
+if not getgenv().Anti_Idle_Controller_Loaded then
+   if getconnections or get_signal_cons then
+      getgenv().notify("Info", "Loading Anti-Idle controller...", 5)
+      local GC = getconnections or get_signal_cons
+
+      getgenv().Anti_Idle_Controller_Loaded = true
+      getgenv().LocalPlayer.Idled:Connect(function()
+         wait(2.5)
+         for i,v in pairs(GC(getgenv().LocalPlayer.Idled)) do
+            if v["Disable"] then
+               v["Disable"](v)
+            elseif v["Disconnect"] then
+               v["Disconnect"](v)
+            end
+         end
+      end)
+    else
+        getgenv().Anti_Idle_Controller_Loaded = true
+   end
+end
+
+if not getgenv().Character then
+    getgenv().Character = Get_Char(getgenv().LocalPlayer)
+end
+getgenv().HumanoidRootPart = SafeGetHRP(getgenv().Character)
+getgenv().Humanoid = SafeGetHumanoid(getgenv().Character)
+getgenv().Head = SafeGetHead(getgenv().Character)
+wait(0.3)
+local function Dynamic_Character_Updater(character)
+	getgenv().Character = character or Get_Char(getgenv().LocalPlayer)
+	wait(0.4)
+   if getgenv().Character and getgenv().Character:FindFirstChild("Humanoid") then
+      getgenv().HumanoidRootPart = SafeGetHRP(character)
+      getgenv().Humanoid = SafeGetHumanoid(character)
+      getgenv().Head = SafeGetHead(character)
+   elseif not getgenv().Character then
+      repeat task.wait() until character and character:FindFirstChild("Humanoid")
+      getgenv().Character = character
+   end
+end
+
+Dynamic_Character_Updater(getgenv().Character)
+task.wait(0.2)
+getgenv().LocalPlayer.CharacterAdded:Connect(function(newCharacter)
+	task.wait(0.2)
+	Dynamic_Character_Updater(newCharacter)
+	repeat wait() until newCharacter:FindFirstChildWhichIsA("Humanoid") and newCharacter:FindFirstChild("HumanoidRootPart")
+	wait(0.4)
+    getgenv().Character = newCharacter or Get_Char(getgenv().LocalPlayer)
+    wait(0.2)
+	getgenv().HumanoidRootPart = SafeGetHRP(newCharacter or getgenv().Character)
+	getgenv().Humanoid = SafeGetHumanoid(newCharacter or getgenv().Character)
+	getgenv().Head = SafeGetHead(newCharacter or getgenv().Character)
+	wait(0.2)
+	Dynamic_Character_Updater(newCharacter or getgenv().Character)
+end)
