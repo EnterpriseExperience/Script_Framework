45dd7d3b32e50dbfc73729d91e12af39bc8939a4
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-10-18 03:47:30 -0400

    Update Life_Together_Network.lua

--- Life_Together_Network.lua
+++ Life_Together_Network.lua
@@ -2,6 +2,54 @@ if not getgenv().Game then
    getgenv().Game = cloneref and cloneref(game) or game
 end
 wait(0.1)
+local Network
+
+local function retrieve_executor()
+   local name
+   if identifyexecutor then
+      name = identifyexecutor()
+   end
+   return { Name = name or "Unknown Executor" }
+end
+
+local function identify_executor()
+   local executorDetails = retrieve_executor()
+   return tostring(executorDetails.Name)
+end
+
+wait(0.1)
+local executor_string = identify_executor()
+
+local function executor_contains(substr)
+   if type(executor_string) ~= "string" then
+      return false
+   end
+
+   return string.find(string.lower(executor_string), string.lower(substr), 1, true) ~= nil
+end
+
+wait(0.1)
+
+if executor_contains("LX63") then
+    local Net
+
+    for _, obj in pairs(getgc(true)) do
+        if typeof(obj) == "table" then
+            if typeof(rawget(obj, "send")) == "function" and typeof(rawget(obj, "get")) == "function" then
+                local info = debug.getinfo(obj.get)
+                if info and info.source and info.source:find("Net") then
+                    Net = obj
+                    break
+                end
+            end
+        end
+    end
+
+    if Net then
+        Network = Net
+    end
+end
+
 local HttpService = cloneref and cloneref(getgenv().Game:GetService("HttpService")) or getgenv().Game:GetService("HttpService")
 local Players = cloneref and cloneref(getgenv().Game:GetService("Players")) or getgenv().Game:GetService("Players")
 local RunService = cloneref and cloneref(getgenv().Game:GetService("RunService")) or getgenv().Game:GetService("RunService")
@@ -12,49 +60,86 @@ local Modules = ReplicatedStorage:WaitForChild("Modules")
 getgenv().Modules = Modules
 local Core = Modules:WaitForChild("Core")
 local Game = Modules:WaitForChild("Game")
-local Invisible_Module = require(Game:FindFirstChild("InvisibleMode"))
-getgenv().Invisible_Module = Invisible_Module
-local Billboard_GUI = require(Game:FindFirstChild("CharacterBillboardGui"))
-getgenv().Billboard_GUI = Billboard_GUI
-local PlotMarker = require(Game:FindFirstChild("PlotMarker"))
-getgenv().PlotMarker = PlotMarker
-local Data = require(Core:FindFirstChild("Data"))
-getgenv().Data = Data
-local Phone_Module = Game:FindFirstChild("Phone")
-getgenv().Phone_Module = Phone_Module
-local Phone = require(Game:FindFirstChild("Phone"))
-getgenv().Phone = Phone
-local Privacy = require(Core:FindFirstChild("Privacy"))
-getgenv().Privacy = Privacy
-local AppModules = Phone_Module:FindFirstChild("AppModules")
-getgenv().AppModules = AppModules
-local Messages = require(AppModules:FindFirstChild("Messages"))
-getgenv().Messages = Messages
-local Network = require(Core:FindFirstChild("Net"))
-local CCTV = require(Game:FindFirstChild("CCTV"))
-getgenv().CCTV = CCTV
-local Tween = require(Core:FindFirstChild("Tween"))
-getgenv().Tween = Tween
-local Modules = ReplicatedStorage:FindFirstChild("Modules")
-wait(0.2)
+wait(0.1)
+if executor_contains("LX63") then
+    local targets = {
+        "InvisibleMode",
+        "CharacterBillboardGui",
+        "PlotMarker",
+        "Data",
+        "Phone",
+        "Privacy",
+        "Messages",
+        "CCTV",
+        "Tween"
+    }
+
+    for _, target in ipairs(targets) do
+        for _, obj in pairs(getgc(true)) do
+            if typeof(obj) == "table" then
+                local info
+                local anyFunc
+
+                for _, v in pairs(obj) do
+                    if typeof(v) == "function" then
+                        info = debug.getinfo(v)
+                        anyFunc = true
+                        break
+                    end
+                end
+
+                if anyFunc and info and info.source and info.source:find(target) then
+                    getgenv()[target] = obj
+                    break
+                end
+            end
+        end
+    end
+else
+    local Invisible_Module = require(Game:FindFirstChild("InvisibleMode"))
+    getgenv().Invisible_Module = Invisible_Module
+    local Billboard_GUI = require(Game:FindFirstChild("CharacterBillboardGui"))
+    getgenv().Billboard_GUI = Billboard_GUI
+    local PlotMarker = require(Game:FindFirstChild("PlotMarker"))
+    getgenv().PlotMarker = PlotMarker
+    local Data = require(Core:FindFirstChild("Data"))
+    getgenv().Data = Data
+    local Phone_Module = Game:FindFirstChild("Phone")
+    getgenv().Phone_Module = Phone_Module
+    local Phone = require(Game:FindFirstChild("Phone"))
+    getgenv().Phone = Phone
+    local Privacy = require(Core:FindFirstChild("Privacy"))
+    getgenv().Privacy = Privacy
+    local AppModules = Phone_Module:FindFirstChild("AppModules")
+    getgenv().AppModules = AppModules
+    local Messages = require(AppModules:FindFirstChild("Messages"))
+    getgenv().Messages = Messages
+    Network = require(Core:FindFirstChild("Net"))
+    local CCTV = require(Game:FindFirstChild("CCTV"))
+    getgenv().CCTV = CCTV
+    local Tween = require(Core:FindFirstChild("Tween"))
+    getgenv().Tween = Tween
+    local Modules = ReplicatedStorage:FindFirstChild("Modules")
+end
+wait(0.4)
 function show_notification(Title, Text, Method, Image)
-   if Method == "Normal" and not Image then
-      Phone.show_notification(tostring(Title), tostring(Text))
-   elseif Method == "Warning" then
-      Phone.show_notification(
-         tostring(Title),
-         tostring(Text),
-         nil,
-         "rbxassetid://13828984843"
-      )
-   elseif Method == "Error" then
-      Phone.show_notification(
-         tostring(Title),
-         tostring(Text),
-         nil,
-         "rbxassetid://14930908086"
-      )
-   end
+    if Method == "Normal" and not Image then
+        Phone.show_notification(tostring(Title), tostring(Text))
+    elseif Method == "Warning" then
+        Phone.show_notification(
+            tostring(Title),
+            tostring(Text),
+            nil,
+            "rbxassetid://13828984843"
+        )
+    elseif Method == "Error" then
+        Phone.show_notification(
+            tostring(Title),
+            tostring(Text),
+            nil,
+            "rbxassetid://14930908086"
+        )
+    end
 end
 wait(0.1)
 getgenv().show_notification = show_notification
