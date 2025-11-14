2f39a626d168745cafc03fd2738cfd96d0ef961c
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-11-14 00:10:12 -0500

    Update LifeTogether_Anti_Staff.lua

--- LifeTogether_Anti_Staff.lua
+++ LifeTogether_Anti_Staff.lua
@@ -2,6 +2,45 @@ if not getgenv().Game then
     getgenv().Game = cloneref and cloneref(game) or game
 end
 
+local HttpService = cloneref and cloneref(game:GetService("HttpService")) or game:GetService("HttpService")
+local players = game:GetService("Players")
+local teleportservice = game:GetService("TeleportService")
+local placeid = game.PlaceId
+local jobid = game.JobId
+local success, latestversioninfo = pcall(function()
+   local vjson = game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/main/Script_Versions_JSON?cachebust=" .. tick())
+   return HttpService:JSONDecode(vjson)
+end)
+
+function getmainraw(v)
+   return string.split(v,"-")[1]
+end
+
+if success and latestversioninfo then
+   local raw = getmainraw(latestversioninfo.LifeTogether_Admin_Version)
+   if raw ~= getgenv().Script_Version_GlobalGenv then
+      getgenv().LifeTogetherRP_Admin = false
+      wait(0.3)
+      for i = 1,2 do
+         task.wait(0.5)
+         players.LocalPlayer.OnTeleport:Connect(function(state)
+            if (not getgenv().TeleportCheck_Admin) and getgenv().queueteleport then
+               getgenv().TeleportCheck_Admin = true
+               queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP_Admin.lua'))()")
+            end
+         end)
+      end
+      task.wait(0.1)
+      if #players:GetPlayers() <= 1 then
+         players.LocalPlayer:Kick("\nRejoining...")
+         task.wait()
+         teleportservice:Teleport(placeid, players.LocalPlayer)
+      else
+         teleportservice:TeleportToPlaceInstance(placeid, jobid, players.LocalPlayer)
+      end
+   end
+end
+
 local Players = cloneref and cloneref(game:GetService("Players")) or game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local GroupId = getgenv().Game.CreatorId
